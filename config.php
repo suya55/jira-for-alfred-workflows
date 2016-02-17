9fadfa62d5baba3591f7f973fdc0d4b78724128d
SeongSu,Kim <suya55@gmail.com>
2016-02-17 16:11:07 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-// JIRA系统域名
+// JIRA 도메인 이름 시스템
 $jiraUrl    = 'http://jira.company.com';
 
-// 登陆JIRA后，将Cookie中的‘seraph.rememberme.cookie’值，替换下面的xxx
+// 쿠키 'seraph.rememberme.cookie'값, JIRA 방문 후 다음 XXX 교체
 $jiraCookie = 'seraph.rememberme.cookie=xxx';
